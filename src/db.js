cc1c292bef4821c926f9494c0a284abcba3f3483
Muthamilan_ram <muthamilanram10@gmail.com>
2024-08-02 19:59:33 +0530

    db config changes

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'password',
-  database: 'ecommerce_db',
+  host: 'database-1.ctqllx5pl3kk.ap-south-1.rds.amazonaws.com', // Updated RDS endpoint
+  user: 'admin', // Username (ensure this user has appropriate permissions)
+  password: 'rootroot', // Updated password
+  database: 'ecommerce_db', // Your database name
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
